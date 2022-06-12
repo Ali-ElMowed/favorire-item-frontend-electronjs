884e1b7809e861861b4b8d875f347bf061c6b03e
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-06-12 14:08:10 +0300

    Styled the signin page

--- renderer.js
+++ renderer.js
@@ -1,5 +1,10 @@
 let category_title = document.getElementsByClassName("category-brief-title")[0];
+let signin= document.getElementsByClassName("signin-nav")[0];
 
 category_title.addEventListener('click', function(){
     window.location.href = "./assets/pages/category/category.html";
-})
+});
+
+signin.addEventListener('click', function(){
+    window.location.href = "./assets/pages/login/login.html";
+});
