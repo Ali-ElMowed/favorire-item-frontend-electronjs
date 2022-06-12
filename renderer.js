0eda6c383cfdd571e3e0de8e891ef77ff19a88ad
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-06-12 05:01:56 +0300

    Added the html part of category.html

---
+++ renderer.js
@@ -0,0 +1,5 @@
+let category_title = document.getElementsByClassName("category-brief-title")[0];
+
+category_title.addEventListener('click', function(){
+    window.location.href = "./assets/pages/category/category.html";
+})
